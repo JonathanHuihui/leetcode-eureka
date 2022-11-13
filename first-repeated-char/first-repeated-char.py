60388be68c2e950141ed5480db827b1382e86afb
Jonathan Huihui <30911328+JonathanHuihui@users.noreply.github.com>
2022-11-13 09:52:04 -0500

    Derp search/replace fuckup (© whatthecommit.com)

--- first-repeated-char/first-repeated-char.py
+++ first-repeated-char/first-repeated-char.py
@@ -2,13 +2,17 @@
 
 class Solution:
     def find_repeats(self, nums: list[int]) -> int:
-        repeated_number = 0
-        return repeated_number
+        ndict = {}
+        for num in nums:
+            if num in ndict:
+                return num
+            ndict[num] = True
+        return None
 
 
 
 sol = Solution()
-sol.find_repeats([1,2,3,2,4,5,6,4,1,2,4])
+print(sol.find_repeats([1,2,3,2,4,5,6,4,1,2,4]))
 
-sol.find_repeats([6,5,7,1,8,2,3,9,4,10])
+print(sol.find_repeats([6,5,7,1,8,2,3,9,4,10]))
 
